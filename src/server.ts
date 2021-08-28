907e0f64b96616b3573807b0f8ba7385b1899440
henrique-ch <ch.henriquevinha@gmail.com>
2021-08-27 22:39:46 -0300

    Create record in elasticsearch

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import getClient from './client/elasticsearch';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response) => {
+app.get('/', async (request: Request, response: Response) => {
 
-    const client = getClient;
+    const client = getClient();
+
+    const result = await client.index({
+        index: 'elastic_teste',
+        type: 'type_elastic_teste',
+        body: {
+            user: 'Henrique',
+            password: '56156819',
+            email: 'ch.henriquevinha@gmail.com'
+        }
+    });
+
+
+    return response.json(result)
 }) 
 
 app.listen(3333, () => console.log(
