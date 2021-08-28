5a593370e060f4118d9e8c8ae85d7741f88807b7
henrique-ch <ch.henriquevinha@gmail.com>
2021-08-28 02:53:43 -0300

    Fix

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import getClient from './client/elasticsearch';
+import DBController from './DBcontroller';
+import PhotoController from './PhotoController';
 
 const app = express();
 
@@ -20,6 +22,15 @@ app.get('/', async (request: Request, response: Response) => {
 
     return response.json(result)
 }) 
+
+
+app.get('/db/create', DBController.create);
+app.get('/photos/create', PhotoController.create);
+app.get('/photos/findAll', PhotoController.findAll);
+app.get('/photos/findById/:id', PhotoController.findById);
+app.get('/photos/createPhoto', PhotoController.createPhoto);
+
+app.get('/photos/findByQuery', PhotoController.findByQuery);
 
 app.listen(3333, () => console.log(
     'Running'
